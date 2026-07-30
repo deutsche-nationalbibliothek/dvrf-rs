c48ed1594308b3f8a19c63bcf6e390e0a2cc448c
Nico Wagner <n.wagner@dnb.de>
2026-07-30 09:01:05 +0200

    ci: setup continuous integration workflow
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,11 @@ pub use level::Level;
 pub use locator::{Locator, LocatorMap, Locators};
 pub use position::Position;
 pub use record::Record;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2, 1 + 1);
+    }
+}
